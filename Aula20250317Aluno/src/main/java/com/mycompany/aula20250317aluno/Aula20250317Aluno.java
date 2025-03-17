63986fd2461079cc3d3028ab717baf99ae76c45c
heitorodrigue <160082563+heitorodrigue@users.noreply.github.com>
2025-03-17 10:19:30 -0300

    Add files via upload

---
+++ Aula20250317Aluno/src/main/java/com/mycompany/aula20250317aluno/Aula20250317Aluno.java
@@ -0,0 +1,17 @@
+package com.mycompany.aula20250317aluno;
+
+public class Aula20250317Aluno {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("ALUNO");
+     
+        Aluno a1 = new Aluno("Cassio", "CC", "B", 3, 7,8);
+        
+        
+        System.out.println(a1);
+        a1.passar("C", 7);
+
+    }
+}
